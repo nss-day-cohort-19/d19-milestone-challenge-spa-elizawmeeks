be48aa6dbb0ecd514aee1342b8147fb80c0af8a2
Eliza Meeks <theartstudio@Elizas-MacBook-Pro.local>
2017-05-02 21:57:38 -0500

    getting started

---
+++ javascripts/carlot.js
@@ -0,0 +1,27 @@
+var CarLot = (function () {
+  var inventory = [];
+
+  return {
+    loadInventory: function (callback) {
+      var inventoryLoader = new XMLHttpRequest();
+
+      inventoryLoader.addEventListener("load", function () {
+      	console.log("file loaded");
+      	var inventoryData = JSON.parse(event.target.responseText);
+      	console.log("inventoryData", inventoryData);
+      	for (var i = 0; i < inventoryData.cars.length; i++){
+      		inventory.push(inventoryData.cars[i]);
+      	}
+      	console.log("inventory array", inventory);
+      });
+
+      inventoryLoader.open("GET", "inventory.json");
+      inventoryLoader.send();
+    }
+
+    getInventory: function (){
+    	return inventory;
+    }
+  };
+
+})(CarLot || {});
---
+++ javascripts/carlot2.js
@@ -0,0 +1,4 @@
+var CarLot = (function () {
+  
+
+})(CarLot || {});
